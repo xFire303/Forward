493a572137672c148a274e07aa35d49cd7831194
filippomeneguzzi <filippomeneguzzi30@gmail.com>
2024-04-10 10:38:48 +0200

    fine animazione navbar

--- js/index.js
+++ js/index.js
@@ -5,7 +5,10 @@ const hamburger = document.getElementById('hamburger');
 
 //show menu
 hamburger.addEventListener('click',()=>{
+
     containerNav.classList.toggle('showMenu');
+    hamburger.classList.toggle('open');
+
 })
 //chiude il menu ogni volta che clicco un link
 linkNav.forEach((link)=>{
